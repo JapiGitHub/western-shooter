7fbe05d79c3ac8a4584875560f9402e99879a9a2
Japi MacKorkee <janne.korkee@gmail.com>
2021-05-28 10:59:07 +0300

    reactio ajan mittaus toimii

--- src/components/TextSplash.jsx
+++ src/components/TextSplash.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./textsplash.scss";
 
 export default function TextSplash({ playerReady, setPlayerReady }) {
+  const [infoText, setInfoText] = useState("Ready?");
+  const [reactionTime, setReactionTime] = useState(7777);
+  const [startTime, setStartTime] = useState(888);
+  const [randomTime, setRandomTime] = useState(0);
+
+  useEffect(() => {
+    setRandomTime(5000 + Math.floor(Math.random() * 3000));
+    console.log(randomTime);
+  }, []);
+
+  useEffect(() => {
+    setStartTime(5000 + Math.floor(Math.random() * 3000));
+    console.log("R", randomTime);
+  }, [randomTime]);
+
+  const readyClick = () => {
+    //START TIMER
+    if (playerReady === false) {
+      setPlayerReady(true);
+      //3000ms kohdalla tulee "set..."
+
+      setInfoText("Ready!");
+
+      setTimeout(() => {
+        setInfoText("Set ...");
+      }, 3000);
+
+      setTimeout((startTime) => {
+        setInfoText("BANG!");
+        console.log(randomTime);
+        setStartTime(new Date());
+      }, randomTime);
+    }
+
+    //SHOOTING
+    if (playerReady === true) {
+      const pullTriggerTime = new Date();
+      setReactionTime(pullTriggerTime - startTime);
+      console.log("reac", pullTriggerTime - startTime);
+      console.log(reactionTime);
+      console.log("startTime:", startTime);
+    }
+  };
+
   return (
-    <div className="textSplashFrame">
-      <div className="infoText">Ready?</div>
+    <div className="textSplashFrame" onClick={readyClick}>
+      <div className="infoText">{infoText}</div>
     </div>
   );
 }
--- src/components/World.jsx
+++ src/components/World.jsx
@@ -6,17 +6,17 @@ import TextSplash from "./TextSplash";
 export default function World() {
   const [playerReady, setPlayerReady] = useState(false);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      console.log("3sek");
-    }, 3000);
-  }, []);
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+
+  //   }, 3000);
+  // }, []);
 
   return (
     <>
       <main className="background-world-arena">
         <section className="ground"></section>
-        <TextSplash playerReady={playerReady} setPlayerRead={setPlayerReady} />
+        <TextSplash playerReady={playerReady} setPlayerReady={setPlayerReady} />
         <PlayerChars />
         <Cactus />
       </main>
