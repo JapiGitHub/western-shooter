9be847745519a4ead617420f4ea64593eb1b869c
Japi MacKorkee <janne.korkee@gmail.com>
2021-07-06 23:00:34 +0300

    css placing buttons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Settings from "./Settings";
+
 import "./menu.scss";
 import useSound from "use-sound";
 import MenuMusicLoboGlueWorm from "../sounds/LoboLocoGlueworm.mp3";
@@ -20,6 +22,8 @@ export default function Menu({
   const [song] = useState(MenuMusicLoboGlueWorm);
   const [play, { stop }] = useSound(song);
   const [menuMusic, setMenuMusic] = useState(false);
+
+  const [showSettings, setShowSettings] = useState(false);
 
   //The AudioContext was not allowed to start. It must be resumed (or created) after a user gesture on the page
   //eli jos teet esim splash startin ja käyttäjän pitää klikata sitä ekana, että pääsee menuun, niin sit toimii
@@ -77,71 +81,24 @@ export default function Menu({
           Multiplayer (development)
         </button>
       </section>
-
-      <div className="graphicsTitle">Graphics</div>
-      <button className="btn theme2Button" onClick={() => setTheme("sepia")}>
-        Oldie
-      </button>
-      <button className="btn theme1Button" onClick={() => setTheme("normal")}>
-        Hyper Realistic
-      </button>
-
       <button
-        className="btn musicToggle"
-        onClick={() => setMenuMusic(!menuMusic)}
+        className="btn settingsButton"
+        onClick={() => setShowSettings(!showSettings)}
       >
-        Music
+        Settings
       </button>
-      <div className="difficultyInfo">
-        <p>{difficulty} ms fatalities</p>{" "}
-        {(() => {
-          switch (difficulty) {
-            case "200":
-              return <>Lucky Luke</>;
-            case "220":
-              return <>Do you feel lucky punk?</>;
-            case "240":
-              return (
-                <>
-                  Ranger,<br></br> Texas Ranger.
-                </>
-              );
-            case "300":
-              return <>Hold my hand.</>;
-            default:
-              return <>Ranger. Texas Ranger.</>;
-          }
-        })()}
-      </div>
 
-      <div></div>
-
-      <div className="difficultyContainer">
-        <button
-          className={difficulty === "200" ? "btn chosenButton" : "btn"}
-          onClick={() => setDifficulty("200")}
-        >
-          Insane
-        </button>
-        <button
-          className={difficulty === "220" ? "btn chosenButton" : "btn"}
-          onClick={() => setDifficulty("220")}
-        >
-          Hard
-        </button>
-        <button
-          className={difficulty === "240" ? "btn chosenButton" : "btn"}
-          onClick={() => setDifficulty("240")}
-        >
-          Normal
-        </button>
-        <button
-          className={difficulty === "300" ? "btn chosenButton" : "btn"}
-          onClick={() => setDifficulty("300")}
-        >
-          Easy
-        </button>
-      </div>
+      <section className="settingsContainer">
+        {showSettings ? (
+          <Settings
+            setTheme={setTheme}
+            setMenuMusic={setMenuMusic}
+            menuMusic={menuMusic}
+            difficulty={difficulty}
+            setDifficulty={setDifficulty}
+          />
+        ) : null}
+      </section>
     </div>
   );
 }
---
+++ src/components/Settings.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+export default function Settings({
+  setTheme,
+  setMenuMusic,
+  menuMusic,
+  difficulty,
+  setDifficulty,
+}) {
+  return (
+    <>
+      <div className="graphicsTitle">Graphics</div>
+      <button className="btn theme2Button" onClick={() => setTheme("sepia")}>
+        Oldie
+      </button>
+      <button className="btn theme1Button" onClick={() => setTheme("normal")}>
+        Hyper Realistic
+      </button>
+      <button
+        className="btn musicToggle"
+        onClick={() => setMenuMusic(!menuMusic)}
+      >
+        Music
+      </button>
+      <div className="difficultyInfo">
+        <p>{difficulty} ms fatalities</p>{" "}
+        {(() => {
+          switch (difficulty) {
+            case "200":
+              return <>Lucky Luke</>;
+            case "220":
+              return <>Do you feel lucky punk?</>;
+            case "240":
+              return (
+                <>
+                  Ranger,<br></br> Texas Ranger.
+                </>
+              );
+            case "300":
+              return <>Hold my hand.</>;
+            default:
+              return <>Ranger. Texas Ranger.</>;
+          }
+        })()}
+      </div>
+      <div></div>
+      <div className="difficultyContainer">
+        <button
+          className={difficulty === "200" ? "btn chosenButton" : "btn"}
+          onClick={() => setDifficulty("200")}
+        >
+          Insane
+        </button>
+        <button
+          className={difficulty === "220" ? "btn chosenButton" : "btn"}
+          onClick={() => setDifficulty("220")}
+        >
+          Hard
+        </button>
+        <button
+          className={difficulty === "240" ? "btn chosenButton" : "btn"}
+          onClick={() => setDifficulty("240")}
+        >
+          Normal
+        </button>
+        <button
+          className={difficulty === "300" ? "btn chosenButton" : "btn"}
+          onClick={() => setDifficulty("300")}
+        >
+          Easy
+        </button>
+      </div>
+    </>
+  );
+}
